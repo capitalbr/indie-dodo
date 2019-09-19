e0dabfa391bea870e553dd2247c0c377ee9902ae
Bradley <bbarnes.me@gmail.com>
2019-09-19 03:25:48 -0500

    Fix categories buttons

--- client/src/components/categories/CategoryIndex.jsx
+++ client/src/components/categories/CategoryIndex.jsx
@@ -18,27 +18,27 @@ const CategoryIndex = () => {
       <h1 className="categories-header">Select a category that interests you!</h1>
       <h4 className="categories-subheader">Discover projects focused on the six major animal groups.</h4>
       <div className="categories-icons">
-        <Link className="categories-campaign-link" to={`/campaigns/5d47c8419850995b6acfa14b`}>
+        <Link className="categories-campaign-link" to={`/campaigns/5d72bc8be995150bbd666670`}>
           < FaFrog id="categories-icons-item"/>
           <h5 className="categories-icons-item-text">AMPHIBIAN</h5>
         </Link>
-        <Link className="categories-campaign-link" to={`/campaigns/5d47c8419850995b6acfa148`}>
+        <Link className="categories-campaign-link" to={`/campaigns/5d72bc8ae995150bbd66666e`}>
           < FaFish id="categories-icons-item" />
           <h5 className="categories-icons-item-text">FISH</h5>
         </Link>
-        <Link className="categories-campaign-link" to={`/campaigns/5d47c8409850995b6acfa147`}>
+        <Link className="categories-campaign-link" to={`/campaigns/5d72bc8ae995150bbd66666c`}>
           < FaHorse id="categories-icons-item" />
           <h5 className="categories-icons-item-text">MAMMAL</h5>
         </Link>
-        <Link className="categories-campaign-link" to={`/campaigns/5d47c8419850995b6acfa14c`}>
+        <Link className="categories-campaign-link" to={`/campaigns/5d7b415a08889c30fa59221e`}>
           < FaSpider id="categories-icons-item" />
           <h5 className="categories-icons-item-text">INVERTEBRATES</h5>
         </Link>
-        <Link className="categories-campaign-link" to={`/campaigns/5d4678219bb59a307c9c2c32`}>
+        <Link className="categories-campaign-link" to={`/campaigns/5d7b415a08889c30fa59221e`}>
           < FaKiwiBird id="categories-icons-item" />
           <h5 className="categories-icons-item-text">BIRD</h5>
         </Link>
-        <Link className="categories-campaign-link" to={`/campaigns/5d4678219bb59a307c9c2c32`}>
+        <Link className="categories-campaign-link" to={`/campaigns/5d72bc8be995150bbd666670`}>
           < FaDragon id="categories-icons-item" />
           <h5 className="categories-icons-item-text">REPTILE</h5>
         </Link>
